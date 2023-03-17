e9d7f87a3bd6dcbac87862466b6d07504f1709c4
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-03-16 17:06:03 -0700

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+
+  let message;
+  if (ride <= 400){
+    message = "This one is on me!";
+  }
+
+  else if (ride > 400 && ride < 2000){
+    message = 'That will be twenty bucks.';
+  }
+
+  else if (ride >= 2000 && ride <= 2500){
+    message = "I will gladly take your thirty bucks.";
+  }
+
+  else 
+    message = "No can do.";
+
+  return message
+  }
+
+
+  scuberGreetingForFeet(2501);
+
+
+
+
+  
+
+function ternaryCheckCity(city){
+  // Write your code here!
+
+return city === 'NYC' ? "Ok, sounds good." : 'No go.';
+
 }
 
-function ternaryCheckCity(){
+ternaryCheckCity('NYC');
+
+
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message;
+
+  switch (tip) {
+
+    case 'generous':
+      message = "Thank you so much."
+        break;
+
+    case  'not as generous':
+      message = "Thank you."
+        break;
+
+    case 'thanks for everything':
+       message = "Bye."
+        break; 
+  } 
+return message
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+switchOnCharmFromTip('generous');
